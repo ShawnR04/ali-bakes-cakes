c3a2e2f7f13ae6c948ad0588f0b6c61bd409f53b
ShawnR04 <shawnrimai04@gmail.com>
2026-04-09 09:01:09 -0400

    Page Test
    This is the initial page test to see that the navigation to those pages works

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -2,6 +2,6 @@
 
 export default function GalleryPage(){
     return(
-        <></>
+        <>gallery</>
     );
 }
--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,6 +2,6 @@
 
 export default function PricingPage(){
     return(
-        <></>
+        <>pricing</>
     );
 }
